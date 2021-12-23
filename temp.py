22ce710ce79e1fc45bc2649db35b13d79d07bec1
Vladimir_Kirichenko <frozenfish@internet.ru>
2021-12-23 20:05:35 +0300

    commit

--- temp.py
+++ temp.py
@@ -1,4 +1,3 @@
-s = 'bana'
-x = 'aba'
-count = []
-print(s[1:].find(x))
+list_array = [161, 182, 161, 154, 176, 170, 167, 171, 170, 174]
+list_array = set(list_array)
+print(sum(list_array) / len(list_array))
